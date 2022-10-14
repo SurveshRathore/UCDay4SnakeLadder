9482127f3a0f7439af1dda0bec663bf5d9c6674d
SurveshRathore <educatedevilboy@gmail.com>
2022-10-14 16:00:32 +0530

    Master program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day4SnakeAndLadder;
+Console.WriteLine("Welcome to the Snake and ladder game. ");
+//Console.WriteLine("Hello, World!");
+//UC1SnakeLadderGame uc1SLG = new();
+//uc1SLG.SnakeLadder();
+//UC2SnakeLadderGameDieRoll uc2Roll = new UC2SnakeLadderGameDieRoll();
+//uc2Roll.GenerateRoll();
+
+//UC3CheckOption uc3Play = new();
+//uc3Play.CheckOption();
+
+//UC4PlayerWin uc4Win = new();
+//uc4Win.PlayerWin();
+
+//UC5ExactWin uc5ExactWin = new();
+//uc5ExactWin.PlayerExactWin();
+
+//UC6DiceRollCount uc6count = new();
+//uc6count.CountDiceRoll();
+
+UC7TwoPlayerGame uc2player = new();
+uc2player.GameWithTwoPlayer();
