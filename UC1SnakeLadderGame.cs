0084a2bf65963a6c02f1cea9638fea13179f03d1
SurveshRathore <educatedevilboy@gmail.com>
2022-10-14 14:42:56 +0530

    UC1 player position

---
+++ UC1SnakeLadderGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4SnakeAndLadder
+{
+    internal class UC1SnakeLadderGame
+    {
+        public void SnakeLadder()
+        {
+            int playerPosition = 0;
+            
+            Console.WriteLine("It's a single player game. ");
+            Console.WriteLine("Single player position is: " + playerPosition);
+        }
+        
+    }
+}
