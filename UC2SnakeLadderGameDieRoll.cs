e8658fedf5450977ebc38220055d9afad22852a8
SurveshRathore <educatedevilboy@gmail.com>
2022-10-14 14:44:14 +0530

    UC2 roll the die

---
+++ UC2SnakeLadderGameDieRoll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4SnakeAndLadder
+{
+    internal class UC2SnakeLadderGameDieRoll
+    {
+        public void GenerateRoll ()
+        {
+            Random random = new Random();
+            int roll = random.Next(1, 7);
+
+            Console.WriteLine("The Player roll is: " + roll);
+        }
+    }
+}
